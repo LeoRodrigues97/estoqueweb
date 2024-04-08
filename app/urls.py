fcbfc7370d65ad67cd47a1889c85b7415a44c2a7
LeoRodrigues97 <68879694+LeoRodrigues97@users.noreply.github.com>
2024-04-08 16:23:09 -0300

    first commit

---
+++ app/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from accounts.views import login_view, logout_view
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', views.home, name='home'),
+    path('', include('suppliers.urls')),
+    path('', include('brands.urls')),
+    path('', include('categories.urls')),
+    path('', include('products.urls')),
+    path('', include('inflows.urls')),
+    path('', include('outflows.urls')),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+]
